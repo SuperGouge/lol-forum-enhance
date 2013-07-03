34303c0e704abb62ac5c0d28699b1c37d8ab49e5
Philipp Widdra <philipp.widdra@googlemail.com>
2013-07-03 08:58:30 +0200

    v0.0.2 - Updates! Minor code changes on userscript. Mostly cleanup and preparation for later changes.

--- HISTORY.md
+++ HISTORY.md
@@ -1,8 +1,22 @@
 Repository Version History
 ==========================
-
-## Current Version: 0.0.1
+### Goals:
 - [ ] Mark nonexistent summoners
 - [ ] Insert summoner icon when quick-replying
-- [ ] Automatic update notifications
+- [?] Automatic update notifications
 - [ ] Show server status
+
+---
+
+## Current Version: 0.0.2
+- mainly cleanup
+- greasemonkey metadata added
+	- should update automatically now
+	- runs at page start now
+	- loads (and caches) `jQuery v2.0.1`
+- basis for clientside level 1 caching with cookies done ( `getCookie(...)` / `setCookie(...)` )
+- Summoner Images added for later use
+
+
+## 0.0.1
+- Initial version
--- client/forum-icons.user.js
+++ client/forum-icons.user.js
@@ -1,67 +1,81 @@
 // ==UserScript==
 // @name        LoL Forum Icons
-// @namespace   ButWhyMe
+// @namespace   https://github.com/philippwiddra
+// @description Supplements the League of Legends forums and sites with additional functions.
 // @include     *.leagueoflegends.com/board/*
-// @version     1
+// @downloadURL https://github.com/philippwiddra/lol-forum-enhance/raw/master/client/forum-icons.user.js
+// @updateURL   https://github.com/philippwiddra/lol-forum-enhance/raw/master/client/forum-icons.user.js
+// @version     0.0.2
+// @run-at      document-start
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min.js
 // ==/UserScript==
 
+function setCookie(name, value, expireDays)
+{
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + expireDays);
+    document.cookie = name + "=" + escape(value) + ((expireDays == null) ? "" : ";expires=" + expireDate.toUTCString());
+}
+
+function getCookie(name)
+{
+	var value = document.cookie;
+	var start = value.indexOf(" " + name + "=");
+	if (start == -1)
+	{
+		start = value.indexOf(name + "=");
+	}
+	if (start == -1)
+	{
+		value = null;
+	}
+	else
+	{
+		start = value.indexOf("=", start) + 1;
+		var end = value.indexOf(";", start);
+		if (end == -1)
+		{
+			end = value.length;
+		}
+		value = unescape(value.substring(start, end));
+	}
+	return value;
+}
+
+//setCookie("testtesttest", "abc", 1);
+//alert(getCookie("testtesttest"));
 
 
 $(document).ready(function () {
 	var callbackFunction = "(function (json) { $('big').filter(function(){ return $(this).text() === json.name;}).parent().find('img.user_summoner_icon').attr('src', 'http://img.lolking.net/shared/riot/images/profile_icons/profileIcon' + json.profileIconId + '.jpg').parent().find('span.left_orb').text(json.summonerLevel); })";
-	//var callbackFunction = "(function (json) { $('big').filter(function(){ return $(this).text() === json.name;}).parent().find('img.user_summoner_icon').attr('src', 'http://www.elophant.com/images/profile-icon/' + json.profileIconId + '.jpg').parent().find('span.left_orb').text(json.summonerLevel); })";
 	callbackFunction = callbackFunction.replace(/\+/g, '%2B');
 	
 	var match = document.URL.match(/(na)|(euw)|(eune)|(br)/i);
-	if (match != null)
+	if (match != null) // Server found
 	{
 		var server = match[0];
-		//alert(server);
 		//$('#lol-pvpnet-bar-inner').append($('<div style="line-height: 30px;">Server found: '+server+'</div>'));
 		
 		// get all Left items except those of rioters.
 		var allLeft = $('.forum_post img.user_summoner_icon').filter($('img[src="lol_theme/img/unknown_icon.jpg"]')).parent().parent().parent().parent();
 
 		allLeft.each(function(i, e) {
-			//var name = encodeURI($(e).find('big').text());
 			var name = $(e).find('big').text();
 			
 			var image = $(e).find('img.user_summoner_icon');
-			var url = 'http://passwd.ohost.de/lcapi/getSummoner.php?summoner=' + name + '&server=' + server + '';
-			
-			//alert(name+' - '+$(e).find('big').text());
-			
-			//.attr('src', 'http://img.lolking.net/shared/riot/images/profile_icons/profileIcon' + json.profileIconId + '.jpg')
-			//.find('img.user_summoner_icon')
-			
+			var url = 'http://passwd.ohost.de/lcapi/getSummoner.php?summoner=' + name + '&server=' + server + '';			
 
 			$.ajax({
 				type: 'GET',
 				url: url,
 				async: true,
 				jsonpCallback: callbackFunction,
-				//contentType: "application/json",
 				contentType: "text/javascript",
 				dataType: 'jsonp',
-				//success: function(json) {
-					//images.push({'img': image, 'src': 'http://img.lolking.net/shared/riot/images/profile_icons/profileIcon' + json.profileIconId + '.jpg'});
-					//alert('name: '+json.name+'\nicon: '+json.profileIconId+'\nlevel: '+json.summonerLevel);
-					//o = new Object();
-					//o.img = image;
-					//o.src = 'http://img.lolking.net/shared/riot/images/profile_icons/profileIcon' + json.profileIconId + '.jpg';
-					//images.push(o);
-					//image.attr('src', "http://img.lolking.net/shared/riot/images/profile_icons/profileIcon1.jpg");
-				//},
-				//error: function(e) {
-				   //alert(e.message);
-				//}
 			});
 
 		});
 	}
-	else
-	{
-		//alert('Server not found.');
-		//$('#lol-pvpnet-bar-inner').append($('<div>Server not found.</div>'));
-	}
+	else // Server not found
+	{}
 });
