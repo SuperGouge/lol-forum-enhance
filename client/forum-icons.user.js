071f372ca373d3f370131deafb02120efb57f687
Philipp Widdra <philipp.widdra@googlemail.com>
2013-07-10 00:24:32 +0200

    Added Userscript and LolForums class.

--- client/forum-icons.user.js
+++ client/forum-icons.user.js
@@ -285,13 +285,71 @@ function Level2Cache()
   }
 }
 
-//var img = document.createElement('img');
-//img.src = GM_getResourceURL("icon2");
-//document.body.appendChild(img);
-//GM_openInTab("http://www.google.de/");
-//GM_info
-//GM_info.version // Greasemonkey-Version
-//GM_registerMenuCommand("Say hi", function() { alert("Hi!"); }, "s");
+function Userscript()
+{
+  var that = this;
+
+  this.addGlobalStyle = function(css)
+  {
+    try
+    {
+      var elmHead, elmStyle;
+      elmHead = document.getElementsByTagName('head')[0];
+      elmStyle = document.createElement('style');
+      elmStyle.type = 'text/css';
+      elmHead.appendChild(elmStyle);
+      elmStyle.innerHTML = css;
+    }
+    catch (e)
+    {
+      if (!document.styleSheets.length)
+      {
+        document.createStyleSheet();
+      }
+      document.styleSheets[0].cssText += css;
+    }
+  }
+  
+  this.setCookie = function(name, value, expireDays)
+  {
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + expireDays);
+    document.cookie = name + "=" + escape(value) + ((expireDays == null) ? "" : ";expires=" + expireDate.toUTCString());
+  }
+  
+  this.getCookie = function(name)
+  {
+    var value = document.cookie;
+    var start = value.indexOf(" " + name + "=");
+    if (start == -1)
+    {
+      start = value.indexOf(name + "=");
+    }
+    if (start == -1)
+    {
+      value = null;
+    }
+    else
+    {
+      start = value.indexOf("=", start) + 1;
+      var end = value.indexOf(";", start);
+      if (end == -1)
+      {
+        end = value.length;
+      }
+      value = unescape(value.substring(start, end));
+    }
+    return value;
+  }
+}
+
+// var img = document.createElement('img');
+// img.src = GM_getResourceURL("icon2");
+// document.body.appendChild(img);
+// GM_openInTab("http://www.google.de/");
+// GM_info
+// GM_info.version // Greasemonkey-Version
+// GM_registerMenuCommand("Say hi", function() { alert("Hi!"); }, "s");
 
 /*
 var level1Cache = new Level1Cache();
@@ -303,115 +361,75 @@ level1Cache.getSummoner("ButWhyMe", "euw", function(s) {
   });
 */
 
-function userscript()
-{
+/*
+var script = new Userscript();
+script.setCookie("test", "0", 1);
+script.alert(getCookie("test"));
+script.addGlobalStyle("p { font-weight: bold; }");
+*/
 
-}
-
-function addGlobalStyle(css)
+function LolForums()
 {
-  try {
-    var elmHead, elmStyle;
-    elmHead = document.getElementsByTagName('head')[0];
-    elmStyle = document.createElement('style');
-    elmStyle.type = 'text/css';
-    elmHead.appendChild(elmStyle);
-    elmStyle.innerHTML = css;
-  } catch (e) {
-    if (!document.styleSheets.length) {
-      document.createStyleSheet();
-    }
-    document.styleSheets[0].cssText += css;
-  }
-}
+  var that = this;
+  
+  this.server = getServer();
 
-function setCookie(name, value, expireDays)
-{
-    var expireDate = new Date();
-    expireDate.setDate(expireDate.getDate() + expireDays);
-    document.cookie = name + "=" + escape(value) + ((expireDays == null) ? "" : ";expires=" + expireDate.toUTCString());
-}
-
-function getCookie(name)
-{
-  var value = document.cookie;
-  var start = value.indexOf(" " + name + "=");
-  if (start == -1)
+  function getServer()
   {
-    start = value.indexOf(name + "=");
-  }
-  if (start == -1)
-  {
-    value = null;
-  }
-  else
-  {
-    start = value.indexOf("=", start) + 1;
-    var end = value.indexOf(";", start);
-    if (end == -1)
+    // get the server from url
+    var match = document.URL.match(/(na)|(euw)|(eune)|(br)/i); // TODO: Add other regions, check compatibility
+    if (match != null)
     {
-      end = value.length;
+      // server found
+      return match[0];
     }
-    value = unescape(value.substring(start, end));
+    else
+    {
+      // server not found
+      return null;
+    }
   }
-  return value;
-}
+  
+  this.countAllPosts = function()
+  {
+    return $('.forum_post img.user_summoner_icon').parent().parent().parent().parent().length;
+  }
+  
+  this.replaceAvatars = function()
+  {
+    var callbackFunction = "(function (json) { $('big').filter(function(){ return $(this).text() === json.name;}).parent().find('img.user_summoner_icon').attr('src', 'http://img.lolking.net/shared/riot/images/profile_icons/profileIcon' + json.profileIconId + '.jpg').parent().find('span.left_orb').text(json.summonerLevel); })";
+    callbackFunction = callbackFunction.replace(/\+/g, '%2B');
 
-function countAllPosts()
-{
-  return $('.forum_post img.user_summoner_icon').parent().parent().parent().parent().length;
-}
+    // get all Left items except those of rioters.
+    var allLeft = $('.forum_post img.user_summoner_icon').filter($('img[src="lol_theme/img/unknown_icon.jpg"]')).parent().parent().parent().parent();
+    allLeft.each(function(i, e) {
+      var name = $(e).find('big').text();
+      var image = $(e).find('img.user_summoner_icon');
 
-function replaceAvatars()
-{
-  var callbackFunction = "(function (json) { $('big').filter(function(){ return $(this).text() === json.name;}).parent().find('img.user_summoner_icon').attr('src', 'http://img.lolking.net/shared/riot/images/profile_icons/profileIcon' + json.profileIconId + '.jpg').parent().find('span.left_orb').text(json.summonerLevel); })";
-  callbackFunction = callbackFunction.replace(/\+/g, '%2B');
+      // TODO: level 1 caching
 
-  // get all Left items except those of rioters.
-  var allLeft = $('.forum_post img.user_summoner_icon').filter($('img[src="lol_theme/img/unknown_icon.jpg"]')).parent().parent().parent().parent();
-  allLeft.each(function(i, e) {
-    var name = $(e).find('big').text();
-    var image = $(e).find('img.user_summoner_icon');
-
-    // TODO: level 1 caching
-
-    // level 2 caching
-    var url = 'http://passwd.ohost.de/lcapi/getSummoner.php?summoner=' + name + '&server=' + server + '';
-    $.ajax({
-      type: 'GET',
-      url: url,
-      async: true,
-      jsonpCallback: callbackFunction,
-      contentType: "text/javascript",
-      dataType: 'jsonp',
+      // level 2 caching
+      var url = 'http://passwd.ohost.de/lcapi/getSummoner.php?summoner=' + name + '&server=' + server + '';
+      $.ajax({
+        type: 'GET',
+        url: url,
+        async: true,
+        jsonpCallback: callbackFunction,
+        contentType: "text/javascript",
+        dataType: 'jsonp',
+      });
     });
-  });
-}
-
-function postCountChanged()
-{
-  replaceAvatars();
-}
-
-function getServer()
-{
-  // get the server from url
-  var match = document.URL.match(/(na)|(euw)|(eune)|(br)/i);
-  if (match != null) // server found
-  {
-    return match[0];
-  }
-  else
-  {
-    return null;
   }
 }
 
 
+/*******************************
+ *    Start of Main Script.    *
+ *******************************/
 
-//setCookie("test", "0", 1);
-//alert(getCookie("test"));
-
+// Initiating main Objects
+var script = new Userscript();
+var forums = new LolForums();
 
 // create an observer for the #posts div instance
 var observerPostsOld = 0;
@@ -419,39 +437,45 @@ var observerPostsInitialized = false;
 var observerTarget = document.querySelector('#posts');
 var postsObserver = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
-    var observerPostsNew = countAllPosts();
-    if ((observerPostsInitialized) && (observerPostsNew != observerPostsOld)) postCountChanged();
+    var observerPostsNew = forums.countAllPosts();
+    if ((observerPostsInitialized) && (observerPostsNew != observerPostsOld))
+    {
+      forums.replaceAvatars();
+    }
   });
 });
 var observerConfig = { childList: true, subtree: true };
 
 // css style changes
-addGlobalStyle("div.panel > div {" + 
-          "min-width: 640px !important;" +
-          "width: auto !important;" +
-          "max-width: none !important;" +
-        "}" +
-        "textarea#vB_Editor_QR_textarea {" +
-          "width: 100% !important;" +
-          "resize: vertical !important;" +
-          "min-height: 100px !important;" +
-        "}");
+script.addGlobalStyle("div.panel > div { \
+                         min-width: 640px !important; \
+                         width: auto !important; \
+                         max-width: none !important; \
+                       } \
+                         textarea#vB_Editor_QR_textarea { \
+                         width: 100% !important; \
+                         resize: vertical !important; \
+                         min-height: 100px !important; \
+                       }");
 
-// find server
-var server = getServer();
-if (server != null) // server found
+
+if (forums.server != null)
 {
+  // server found
+
   // replace the summoner images and levels
-  replaceAvatars();
+  forums.replaceAvatars();
 
   // initialize observer counts and mark as ready
-  observerPostsOld = countAllPosts();
+  observerPostsOld = forums.countAllPosts();
   observerPostsInitialized = true;
 
   // start observing #posts
   postsObserver.observe(observerTarget, observerConfig);
 }
-else // server not found
+else
 {
+  // server not found
+
   // ...
 }
