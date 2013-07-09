ab75082c18f112bd6621965d069c93fcff864319
Philipp Widdra <philipp.widdra@googlemail.com>
2013-07-09 19:34:49 +0200

    Caching done. Tabs replaced.

--- client/forum-icons.user.js
+++ client/forum-icons.user.js
@@ -143,20 +143,20 @@
 
 function Summoner(jsonData)
 {
-	var that = this;
-	
-	// form:
-	/* {
-		"internalName": "butwhyme",
-		"name": "ButWhyMe",
-		"server": "euw",
-		"acctId": 24911708,
-		"profileIconId": 546,
-		"revisionDate": "2013-07-08 02:04:11",
-		"currentDate": "2013-07-08 12:12:01",
-		"summonerLevel": 30,
-		"summonerId": 21405713
-	} */
+  var that = this;
+
+  // form:
+  /* {
+    "internalName": "butwhyme",
+    "name": "ButWhyMe",
+    "server": "euw",
+    "acctId": 24911708,
+    "profileIconId": 546,
+    "revisionDate": "2013-07-08 02:04:11",
+    "currentDate": "2013-07-08 12:12:01",
+    "summonerLevel": 30,
+    "summonerId": 21405713
+  } */
   this.data = {
     internalName : null,
     name : null,
@@ -168,14 +168,14 @@ function Summoner(jsonData)
     summonerLevel : null,
     summonerId : null
   };
-	this.toJsonString = function()
-	{
-		return JSON.stringify(that.data);
-	}
-	this.fromJsonString = function(json)
-	{
-		that.data = JSON.parse(json);
-	}
+  this.toJsonString = function()
+  {
+    return JSON.stringify(that.data);
+  }
+  this.fromJsonString = function(json)
+  {
+    that.data = JSON.parse(json);
+  }
 }
 
 function Level1Cache()
@@ -185,8 +185,8 @@ function Level1Cache()
   var cachedSummonerString = "SummonerCache";
   var level2Cache = new Level2Cache();
 
-	this.getSummoner = function(name, server, done)
-	{
+  this.getSummoner = function(name, server, done)
+  {
     var result = null;
     for (var i = 0; i < cachedSummoners.length; i++)
     {
@@ -220,7 +220,7 @@ function Level1Cache()
         done(summoner);
       });
     }
-	}
+  }
   
   this.loadCache = function()
   {
@@ -269,20 +269,19 @@ function Level1Cache()
 
 function Level2Cache()
 {
-    var getSummonerUrl = "http://passwd.ohost.de/lcapi/getSummoner.php";
-	
-	this.getSummoner = function(name, server, done)
-	{
-		GM_xmlhttpRequest({
-			method: "GET",
-			url: getSummonerUrl + "?summoner=" + name + "&server=" + server + "",
-			onload: function(response) {
-				var s = new Summoner();
+  var getSummonerUrl = "http://passwd.ohost.de/lcapi/getSummoner.php";
+  this.getSummoner = function(name, server, done)
+  {
+    GM_xmlhttpRequest({
+      method: "GET",
+      url: getSummonerUrl + "?summoner=" + name + "&server=" + server + "",
+      onload: function(response) {
+        var s = new Summoner();
         s.fromJsonString(response.responseText);
-				done(s);
-			}
-		});
-	}
+        done(s);
+      }
+    });
+  }
 }
 
 //var img = document.createElement('img');
@@ -306,19 +305,19 @@ level1Cache.getSummoner("ButWhyMe", "euw", function(s) {
 
 function addGlobalStyle(css)
 {
-	try {
-		var elmHead, elmStyle;
-		elmHead = document.getElementsByTagName('head')[0];
-		elmStyle = document.createElement('style');
-		elmStyle.type = 'text/css';
-		elmHead.appendChild(elmStyle);
-		elmStyle.innerHTML = css;
-	} catch (e) {
-		if (!document.styleSheets.length) {
-			document.createStyleSheet();
-		}
-		document.styleSheets[0].cssText += css;
-	}
+  try {
+    var elmHead, elmStyle;
+    elmHead = document.getElementsByTagName('head')[0];
+    elmStyle = document.createElement('style');
+    elmStyle.type = 'text/css';
+    elmHead.appendChild(elmStyle);
+    elmStyle.innerHTML = css;
+  } catch (e) {
+    if (!document.styleSheets.length) {
+      document.createStyleSheet();
+    }
+    document.styleSheets[0].cssText += css;
+  }
 }
 
 function setCookie(name, value, expireDays)
@@ -330,77 +329,77 @@ function setCookie(name, value, expireDays)
 
 function getCookie(name)
 {
-	var value = document.cookie;
-	var start = value.indexOf(" " + name + "=");
-	if (start == -1)
-	{
-		start = value.indexOf(name + "=");
-	}
-	if (start == -1)
-	{
-		value = null;
-	}
-	else
-	{
-		start = value.indexOf("=", start) + 1;
-		var end = value.indexOf(";", start);
-		if (end == -1)
-		{
-			end = value.length;
-		}
-		value = unescape(value.substring(start, end));
-	}
-	return value;
+  var value = document.cookie;
+  var start = value.indexOf(" " + name + "=");
+  if (start == -1)
+  {
+    start = value.indexOf(name + "=");
+  }
+  if (start == -1)
+  {
+    value = null;
+  }
+  else
+  {
+    start = value.indexOf("=", start) + 1;
+    var end = value.indexOf(";", start);
+    if (end == -1)
+    {
+      end = value.length;
+    }
+    value = unescape(value.substring(start, end));
+  }
+  return value;
 }
 
 function countAllPosts()
 {
-	return $('.forum_post img.user_summoner_icon').parent().parent().parent().parent().length;
+  return $('.forum_post img.user_summoner_icon').parent().parent().parent().parent().length;
 }
 
 function replaceAvatars()
 {
-	var callbackFunction = "(function (json) { $('big').filter(function(){ return $(this).text() === json.name;}).parent().find('img.user_summoner_icon').attr('src', 'http://img.lolking.net/shared/riot/images/profile_icons/profileIcon' + json.profileIconId + '.jpg').parent().find('span.left_orb').text(json.summonerLevel); })";
-	callbackFunction = callbackFunction.replace(/\+/g, '%2B');
+  var callbackFunction = "(function (json) { $('big').filter(function(){ return $(this).text() === json.name;}).parent().find('img.user_summoner_icon').attr('src', 'http://img.lolking.net/shared/riot/images/profile_icons/profileIcon' + json.profileIconId + '.jpg').parent().find('span.left_orb').text(json.summonerLevel); })";
+  callbackFunction = callbackFunction.replace(/\+/g, '%2B');
 
-	// get all Left items except those of rioters.
-	var allLeft = $('.forum_post img.user_summoner_icon').filter($('img[src="lol_theme/img/unknown_icon.jpg"]')).parent().parent().parent().parent();
-	allLeft.each(function(i, e) {
-		var name = $(e).find('big').text();
-		var image = $(e).find('img.user_summoner_icon');
-		
-		// TODO: level 1 caching
-		
-		// level 2 caching
-		var url = 'http://passwd.ohost.de/lcapi/getSummoner.php?summoner=' + name + '&server=' + server + '';			
-		$.ajax({
-			type: 'GET',
-			url: url,
-			async: true,
-			jsonpCallback: callbackFunction,
-			contentType: "text/javascript",
-			dataType: 'jsonp',
-		});
-	});
+  // get all Left items except those of rioters.
+  var allLeft = $('.forum_post img.user_summoner_icon').filter($('img[src="lol_theme/img/unknown_icon.jpg"]')).parent().parent().parent().parent();
+  allLeft.each(function(i, e) {
+    var name = $(e).find('big').text();
+    var image = $(e).find('img.user_summoner_icon');
+
+    // TODO: level 1 caching
+
+    // level 2 caching
+    var url = 'http://passwd.ohost.de/lcapi/getSummoner.php?summoner=' + name + '&server=' + server + '';
+    $.ajax({
+      type: 'GET',
+      url: url,
+      async: true,
+      jsonpCallback: callbackFunction,
+      contentType: "text/javascript",
+      dataType: 'jsonp',
+    });
+  });
 }
 
 function postCountChanged()
 {
-	replaceAvatars();
+  replaceAvatars();
 }
 
 function getServer()
 {
-	// get the server from url
-	var match = document.URL.match(/(na)|(euw)|(eune)|(br)/i);
-	if (match != null) // server found
-	{
-		return match[0];
-	}
-	else
-	{
-		return null;
-	}
+  // get the server from url
+  var match = document.URL.match(/(na)|(euw)|(eune)|(br)/i);
+  if (match != null) // server found
+  {
+    return match[0];
+  }
+  else
+  {
+    return null;
+  }
 }
 
 
@@ -414,40 +413,40 @@ var observerPostsOld = 0;
 var observerPostsInitialized = false;
 var observerTarget = document.querySelector('#posts');
 var postsObserver = new MutationObserver(function(mutations) {
-	mutations.forEach(function(mutation) {
-		var observerPostsNew = countAllPosts();
-		if ((observerPostsInitialized) && (observerPostsNew != observerPostsOld)) postCountChanged();
-	});
+  mutations.forEach(function(mutation) {
+    var observerPostsNew = countAllPosts();
+    if ((observerPostsInitialized) && (observerPostsNew != observerPostsOld)) postCountChanged();
+  });
 });
 var observerConfig = { childList: true, subtree: true };
 
 // css style changes
 addGlobalStyle("div.panel > div {" + 
-					"min-width: 640px !important;" +
-					"width: auto !important;" +
-					"max-width: none !important;" +
-				"}" +
-				"textarea#vB_Editor_QR_textarea {" +
-					"width: 100% !important;" +
-					"resize: vertical !important;" +
-					"min-height: 100px !important;" +
-				"}");
+          "min-width: 640px !important;" +
+          "width: auto !important;" +
+          "max-width: none !important;" +
+        "}" +
+        "textarea#vB_Editor_QR_textarea {" +
+          "width: 100% !important;" +
+          "resize: vertical !important;" +
+          "min-height: 100px !important;" +
+        "}");
 
 // find server
 var server = getServer();
 if (server != null) // server found
 {
-	// replace the summoner images and levels
-	replaceAvatars();
-	
-	// initialize observer counts and mark as ready
-	observerPostsOld = countAllPosts();
-	observerPostsInitialized = true;
-	
-	// start observing #posts
-	postsObserver.observe(observerTarget, observerConfig);
+  // replace the summoner images and levels
+  replaceAvatars();
+
+  // initialize observer counts and mark as ready
+  observerPostsOld = countAllPosts();
+  observerPostsInitialized = true;
+
+  // start observing #posts
+  postsObserver.observe(observerTarget, observerConfig);
 }
 else // server not found
 {
-	// ...
+  // ...
 }
