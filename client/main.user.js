e9b3a73eb3fc48608166ca967175ea71194b4c1b
Philipp Widdra <philipp.widdra@googlemail.com>
2013-07-11 02:23:06 +0200

    global.js added and referenced

--- client/main.user.js
+++ client/main.user.js
@@ -19,6 +19,7 @@
 // @grant       GM_registerMenuCommand
 // @resource    globalcss https://raw.github.com/philippwiddra/lol-forum-enhance/master/client/global.css
 // @require     http://code.jquery.com/jquery-2.0.2.min.js
+// @require     https://raw.github.com/philippwiddra/lol-forum-enhance/master/client/global.js
 // @resource    iconUnknown http://passwd.ohost.de/lcapi/SummonerIcons/unknown.jpg
 // @resource    icon0 http://passwd.ohost.de/lcapi/SummonerIcons/0.jpg
 // @resource    icon1 http://passwd.ohost.de/lcapi/SummonerIcons/1.jpg
