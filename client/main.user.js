75c03c5bcb424fe1f7836b474041e86f1ba3a098
Philipp Widdra <philipp.widdra@googlemail.com>
2013-07-25 03:05:25 +0200

    Summoner data caches reworked as objects; fixed level1Cache.cleanCache()

--- client/main.user.js
+++ client/main.user.js
@@ -232,98 +232,12 @@ function Summoner() {
   }
 }
 
-function Level1Cache() {
-  var that = this;
-  var cachedSummoners = {};
-  var cachedSummonerString = "SummonerCache";
-  var level2Cache = new Level2Cache();
-
-  this.getSummoner = function(name, server, found, notFound) {
-    if (typeof cachedSummoners[name] != "undefined") {
-      if (cachedSummoners[name].data.success) found(cachedSummoners[name]);
-      else notFound(cachedSummoners[name]);
-    }
-    else {
-      // Perform a level2Cache call
-      level2Cache.getSummoner(name, server,
-      function(s) {
-        // summoner found
-        
-        // save summoner for this function
-        var summoner = s;
-        
-        // add summoner to level1Cache
-        addSummoner(summoner);
-        
-        // return summoner normally
-        found(summoner);
-      },
-      function(s) {
-        // summoner not found
-        
-        // save summoner for this function
-        var summoner = s;
-        
-        // add summoner to level1Cache
-        addSummoner(summoner);
-        
-        // callback
-        notFound(s);
-      });
-    }
-  }
-  
-  this.loadCache = function() {
-    try {
-      cachedSummoners = JSON.parse(GM_getValue(cachedSummonerString, "{}"));
-    }
-    catch (e) {
-      if (e instanceof SyntaxError) {
-        // error in JSON.parse (input may be not valid JSON)
-        cachedSummoners = {};
-      }
-      else {
-        throw e;
-      }
-    }
-  }
-  
-  this.cleanCache = function() {
-    // get yesterdays Date
-    var d = new Date();
-    d.setDate(d.getDate() - 1);
-    var temp = {};
-    for (var key in cachedSummoners) {
-      if (cachedSummoners.hasOwnProperty(key)) {
-        // is Summonerdata not older than a day
-        if (cachedSummoners[key].data.currentDate > d) {
-          temp[key] = cachedSummoners[key];
-        }
-      }
-    }
-    // only persist newer summoners
-    cachedSummoners = temp;
-  }
-  
-  this.removeCache = function() {
-    GM_deleteValue(cachedSummonerString);
-  }
-  
-  this.saveCache = function() {
-    GM_setValue(cachedSummonerString, JSON.stringify(cachedSummoners));
-  }
-  
-  function addSummoner(summoner) {
-    cachedSummoners[summoner.data.name] = summoner;
-  }
-}
-
-function Level2Cache() {
-  var getSummonerUrl = 'http://www.piltover-libraries.net/lol-forum-enhance/getSummoner.php';
-  this.getSummoner = function(name, server, found, notFound) {
+var level2Cache = {
+  getSummonerUrl: 'http://www.piltover-libraries.net/lol-forum-enhance/getSummoner.php',
+  getSummoner: function(name, server, found, notFound) {
     GM_xmlhttpRequest({
       method: 'GET',
-      url: getSummonerUrl + '?summoner=' + encodeURIComponent(name) + '&server=' + server,
+      url: this.getSummonerUrl + '?summoner=' + encodeURIComponent(name) + '&server=' + server,
       onload: function(response) {
         var s = new Summoner();
         s.fromJsonString(response.responseText);
@@ -331,6 +245,72 @@ function Level2Cache() {
         else notFound(s);
       }
     });
+  }
+}
+
+var level1Cache = {
+  cachedSummoners: {},
+  cachedSummonerString: 'SummonerCache',
+  getSummoner: function(name, server, found, notFound) {
+    if (typeof this.cachedSummoners[name] != 'undefined') {
+      if (this.cachedSummoners[name].data.success) found(this.cachedSummoners[name]);
+      else notFound(this.cachedSummoners[name]);
+    }
+    else {
+      // Perform a level2Cache call
+      level2Cache.getSummoner(name, server, function(s) {
+        // Summoner found:
+        var summoner = s; // save summoner for this function
+        this.addSummoner(summoner); // add summoner to level1Cache
+        found(summoner); // return summoner normally
+      },
+      function(s) {
+        // Summoner not found:
+        var summoner = s; // save summoner for this function
+        this.addSummoner(summoner); // add summoner to level1Cache
+        notFound(s); // callback
+      });
+    }
+  },
+  loadCache: function() {
+    try {
+      this.cachedSummoners = JSON.parse(GM_getValue(this.cachedSummonerString, '{}'));
+    }
+    catch (e) {
+      if (e instanceof SyntaxError) {
+        // error in JSON.parse (input may be not valid JSON)
+        this.cachedSummoners = {};
+      }
+      else {
+        throw e;
+      }
+    }
+  },
+  cleanCache: function() {
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1); // get yesterdays Date
+    var temp = {};
+    for (var key in this.cachedSummoners) {
+      var match = this.cachedSummoners[key].data.currentDate.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/);
+      var currentDate = new Date((+match[1]), (+match[2])-1, (+match[3]), (+match[4]), (+match[5]), (+match[6]));
+      if (this.cachedSummoners.hasOwnProperty(key)) {
+        // is Summonerdata not older than a day?
+        if (currentDate > yesterday) {
+          temp[key] = this.cachedSummoners[key];
+        }
+      }
+    }
+    this.cachedSummoners = temp; // only persist newer summoners
+  },
+  removeCache: function() {
+    GM_deleteValue(this.cachedSummonerString);
+    this.cachedSummoners = {};
+  },
+  saveCache: function() {
+    GM_setValue(this.cachedSummonerString, JSON.stringify(this.cachedSummoners));
+  },
+  addSummoner: function(summoner) {
+    this.cachedSummoners[summoner.data.name] = summoner;
   }
 }
 
@@ -636,7 +616,6 @@ function Userscript() {
 
 function LolForums() {
   var that = this;
-  var level1Cache = new Level1Cache();
   level1Cache.loadCache();
   level1Cache.cleanCache();
   
@@ -1315,6 +1294,7 @@ forums.registerMenuCommands(script);
 var observerTarget = document.querySelector('#posts');
 var postsObserver = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
+    // TODO: Check if forEach is needed, replaceAvatars itself traverses over posts.
     forums.replaceAvatars();
   });
 });
