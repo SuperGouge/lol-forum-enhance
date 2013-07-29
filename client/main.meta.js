36349be7e28727c07cbc5e8f67e415eab94820f7
Philipp Widdra <philipp.widdra@googlemail.com>
2013-07-29 13:34:54 +0200

    v1.0.1

--- client/main.meta.js
+++ client/main.meta.js
@@ -5,7 +5,7 @@
 // @include     *.leagueoflegends.com/board/*
 // @downloadURL https://raw.github.com/philippwiddra/lol-forum-enhance/master/client/main.user.js
 // @updateURL   https://raw.github.com/philippwiddra/lol-forum-enhance/master/client/main.meta.js
-// @version     1.0.0
+// @version     1.0.1
 // @run-at      document-end
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getResourceText
