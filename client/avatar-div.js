7761c72a0d22020d4ea3922052909a59d3935f53
Philipp Widdra <philipp.widdra@googlemail.com>
2013-07-31 01:28:16 +0200

    Own avatar changed

--- client/avatar-div.js
+++ client/avatar-div.js
@@ -1,18 +1,16 @@
 ﻿var avatarDiv = {
     attach: function () {
-        var quickReplyContainer = $("#vB_Editor_QR").parent().parent();
-
-        var avatar = GM_getResourceText('avatardivhtml');
-        
-        quickReplyContainer.prepend(avatar);
-        quickReplyContainer.append($('<div class="userscript-clear"></div>'));
+        var editor = $('#collapseobj_quick_reply .editor-container');
+        var avatar = GM_getResourceText('avatardivhtml'); // TODO: Edit for beta forum
+        $('<div class="userscript-clear"></div>').insertAfter(editor);
+        avatar.insertBefore(editor);
     },
     replaceData: function () {
         var server = riot.getForumServer();
         var name = riot.getOwnForumName();
         var e = $('#userscript-avatar-name');
         if ((!e.data('replaced')) && (name !== null)) {
-            if (lfeOptions.data.charset) name = _from_utf8(name); // charset encoding bugfixes for league forums
+            //if (lfeOptions.data.charset) name = _from_utf8(name); // charset encoding bugfixes for league forums
             var subtitle = localizations.get('avatarSub'); // get localized subtitle
             $('#userscript-avatar-subtitle').text(subtitle); // replace subtitle
             e.text(name); // replace name
@@ -26,44 +24,44 @@
                 // TODO: Add code to handle not found summoner data.
             });
             if (lfeOptions.data.link === 'selection') {
-                e.contents().replaceWith('<button class="btn btn-link userscript-name-button">' + name + '</button>');
-                e.clickover({
-                    content: '<div class="btn-group btn-group-vertical">' +
-                                  '<button class="btn btn-small summoner-clickover" style="width: 160px" type="button" data-href="http://' + server + '.leagueoflegends.com/board/search.php?do=process&searchuser=' + name + '&exactname=1&showposts=1">' + localizations.get('nameClickoverPostsCaption') + '</button>' +
-                                  '<button class="btn btn-small summoner-clickover" style="width: 160px" type="button" data-href="http://' + server + '.leagueoflegends.com/board/search.php?do=process&searchuser=' + name + '&exactname=1&starteronly=1&showposts=0">' + localizations.get('nameClickoverThreadsCaption') + '</button>' +
-                              '</div>' +
-                              '<button id="userscript-clickover-close" style="display: none;" data-toggle="button" data-dismiss="clickover">Close</button>',
-                    animation: true,
+                e.addClass('userscript-name');
+                e.lfePopover({
                     html: true,
-                    placement: 'top',
-                    esc_close: 'false',
-                    onShown: function () {
-                        $('.summoner-clickover').on('click', function () {
-                            var link = $(this).attr('data-href');
-                            GM_openInTab(link);
-                            $('#userscript-clickover-close').click();
-                        });
-                    }
+                    content: '<div class="userscript-summoner-popover-buttons">' +
+                                  '<button type="button" data-href="http://forums.' + server + '.leagueoflegends.com/board/search.php?do=process&searchuser=' + name + '&exactname=1&showposts=1">' + localizations.get('nameClickoverPostsCaption') + '</button>' + // TODO: Check if URL still works when live
+                                  '<button type="button" data-href="http://forums.' + server + '.leagueoflegends.com/board/search.php?do=process&searchuser=' + name + '&exactname=1&starteronly=1&showposts=0">' + localizations.get('nameClickoverThreadsCaption') + '</button>' + // TODO: Check if URL still works when live
+                             '</div>',
+                });
+                e.click(function () {
+                    $(this).lfePopover('toggle');
+                });
+                e.parent().find('.userscript-summoner-popover-buttons > button').click(function () {
+                    var link = $(this).attr('data-href');
+                    GM_openInTab(link);
+                    e.lfePopover('hide');
                 });
             }
             else if (lfeOptions.data.link === 'posts') {
-                e.contents().replaceWith('<button class="btn btn-link userscript-name-button" data-href="http://' + server + '.leagueoflegends.com/board/search.php?do=process&searchuser=' + name + '&exactname=1&showposts=1">' + name + '</button>');
+                e.text(name);
+                e.addClass('userscript-name');
+                e.attr('data-href', 'http://forums.' + server + '.leagueoflegends.com/board/search.php?do=process&searchuser=' + name + '&exactname=1&showposts=1'); // TODO: Check if URL still works when live
                 e.click(function () {
-                    var link = $(this).find('.userscript-name-button').attr('data-href');
+                    var link = $(this).attr('data-href');
                     GM_openInTab(link);
                 });
             }
             else if (lfeOptions.data.link === 'threads') {
-                e.contents().replaceWith('<button class="btn btn-link userscript-name-button" data-href="http://' + server + '.leagueoflegends.com/board/search.php?do=process&searchuser=' + name + '&exactname=1&starteronly=1&showposts=0">' + name + '</button>');
+                e.text(name);
+                e.addClass('userscript-name');
+                e.attr('data-href', 'http://forums.' + server + '.leagueoflegends.com/board/search.php?do=process&searchuser=' + name + '&exactname=1&starteronly=1&showposts=0'); // TODO: Check if URL still works when live
                 e.click(function () {
-                    var link = $(this).find('.userscript-name-button').attr('data-href');
+                    var link = $(this).attr('data-href');
                     GM_openInTab(link);
                 });
             }
             else { // lfeOptions.data.link === 'none'
                 e.text(name);
             }
-
             e.data('replaced', true);
         }
     }
