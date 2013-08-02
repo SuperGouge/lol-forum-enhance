1358dc939214f632578bee090a909b591308d54a
Philipp Widdra <philipp.widdra@googlemail.com>
2013-08-02 22:37:57 +0200

    updates modal localization added; bugfixes

--- client/update-modal.js
+++ client/update-modal.js
@@ -1,6 +1,6 @@
 ﻿var updateModal = {
     addButton: function () {
-        var button = $('<a id="userscript-updates-button" href="#">LFE Updates available!</a>'); // TODO: localization
+        var button = $('<a id="userscript-updates-button" href="#">LFE updates available!</a>'); // TODO: localization
         $('#website-feedback').append(button);
         $('#userscript-updates-button').on('click', function () {
             $('#lfeUpdatesModal').trigger('openModal');
@@ -25,11 +25,6 @@
                 //$(modal).append('Closed!');
             }
         });
-        //$('#lfeUpdatesModal').css('top', 'auto');
-        //$('#lfeUpdatesModal').css('left', 'auto');
-        //$('#lfeUpdatesModal').css('right', '10px');
-        //$('#lfeUpdatesModal').css('bottom', '30px');
-        //$('#lfeUpdatesModal').next().css('pointer-events', 'none');
 
         $('#lfe-update-dismiss').on('click', function () {
             //userscript.setCookie('lfe-update-dismissed', 'true', 60 * 60 * 1000);
@@ -40,13 +35,21 @@
         });
     },
     localize: function () {
-        // TODO: localization
+        $('#lfe-u-captions-button-dismiss').text(localizations.get('updateButtonDismiss'));
+        $('#lfe-u-captions-button-update').text(localizations.get('updateButtonUpdate'));
+        $('#lfe-u-captions-new-version').text(localizations.get('updateNewVersionCaption'));
+        $('#lfe-u-captions-own-version').text(localizations.get('updateOwnVersionCaption'));
+        $('#lfe-u-captions-text').text(localizations.get('updateTextCaption'));
+        $('#lfe-u-captions-title').text(localizations.get('updateTitleCaption'));
+        $('#userscript-updates-button').text(localizations.get('updateAvailableButton'));
     },
     showIfNeccessary: function () {
         var dismissed = userscript.getCookie('lfe-update-dismissed');
         if (lfeOptions.data.updates && !dismissed) {
             userscript.updateNeccessary(function (updateNecc) {
                 if (updateNecc) {
+                    $('#userscript-own-version').test(userscript.getLocalVersion());
+                    $('#userscript-new-version').text(userscript.getRemoteVersion());
                     $('#userscript-updates-button').css('display', 'block');
                 }
             });
